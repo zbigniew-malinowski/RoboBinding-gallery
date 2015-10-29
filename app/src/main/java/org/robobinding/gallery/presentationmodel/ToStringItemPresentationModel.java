30b8d8ddc8a38946d45df7817a8a503ba8ebab05
Zbigniew Malinowski <zbigniew.malinowski@partners.mbank.pl>
2015-10-29 12:37:43 +0100

    Added workaround suggested for Robobinding #238 (change support for ItemPresentationModel)

--- app/src/main/java/org/robobinding/gallery/presentationmodel/ToStringItemPresentationModel.java
+++ app/src/main/java/org/robobinding/gallery/presentationmodel/ToStringItemPresentationModel.java
@@ -2,21 +2,34 @@ package org.robobinding.gallery.presentationmodel;
 
 import org.robobinding.itempresentationmodel.ItemContext;
 import org.robobinding.itempresentationmodel.ItemPresentationModel;
+import org.robobinding.presentationmodel.HasPresentationModelChangeSupport;
+import org.robobinding.presentationmodel.PresentationModelChangeSupport;
 
 /**
  * @author Cheng Wei
  * @version $Revision: 1.0 $
  * @since 1.0
  */
-public class ToStringItemPresentationModel implements ItemPresentationModel<Object> {
+public class ToStringItemPresentationModel implements ItemPresentationModel<Object>, HasPresentationModelChangeSupport {
+    private final PresentationModelChangeSupport changeSupport;
     private Object item;
+
+    public ToStringItemPresentationModel() {
+        changeSupport = new PresentationModelChangeSupport(this);
+    }
 
     @Override
     public void updateData(Object item, ItemContext itemContext) {
         this.item = item;
+        changeSupport.refreshPresentationModel();
     }
 
     public String getValue() {
         return item.toString();
+    }
+
+    @Override
+    public PresentationModelChangeSupport getPresentationModelChangeSupport() {
+        return changeSupport;
     }
 }
